a1c0055a65df1b22718e0ede0a53b8ec0733a089
oohira <t3e0vbbdwbd0zr6bgftu@docomo.ne.jp>
2018-12-06 03:44:39 +0900

    タスク：カードをデータで管理するシステムを作る
    大平
    ・CSVファイルからカードデータを読み込む各カードに反映する機能を作成しました
    ・カードIDからカード画像を読み込むようにしました

--- WarConVer.TGS/Assets/CardDataLoader.cs
+++ WarConVer.TGS/Assets/CardDataLoader.cs
@@ -7,42 +7,63 @@ using System.IO;
 //
 //==使用方法：カードデータを読み込むGameObjectにアタッチ
 public class CardDataLoader : MonoBehaviour {
+	[SerializeField] string[] _csvFimeNames = null;		//CSVファイル名の配列
 	[SerializeField] List<CardData> _cardDatas = null;	//カードデータ構造体リスト
-	[SerializeField] List<string[]> _csvStringData = new List<string[]>();
-	[SerializeField] TextAsset _csvData = null;
 
 	void Awake() {
-		_csvData = (TextAsset)Resources.Load ("CSV/black_card_data");
-		Debug.Log (_csvData.text);
-		StringReader stringReader = new StringReader (_csvData.text);
-		while (stringReader.Peek () > -1) {
-			//','毎に区切って配列へ格納
-			string line = stringReader.ReadLine();
-			_csvStringData.Add (line.Split(','));
+		//CSVからカードデータを読み込む処理------------------
+		for (int i = 0; i < _csvFimeNames.Length; i++) {
+			LoadFromCSV ( _csvFimeNames[i] );
 		}
-		for (int i = 0; i < _csvStringData.Count; i++) {
+		//-------------------------------------------------
+	}
+
+
+	//--CSVからカードデータを読み込む関数
+	void LoadFromCSV( string csvFimeName ) {
+		TextAsset csvTextAsset = (TextAsset)Resources.Load ("CSV/" + csvFimeName);	//CSVファイルをTextAssetとして読み込む
+		StringReader stringReader = new StringReader (csvTextAsset.text);			//csvTextAssetのテキストを読むStringReaderを生成
+		List<string[]> csvStringArrayList = new List<string[]> ();					//CSVの各値を列ごとにstring配列で格納するための変数
+		//カンマごとに区切って配列へ格納-------------------
+		while (stringReader.Peek () > -1) {
+			string line = stringReader.ReadLine();
+			csvStringArrayList.Add (line.Split(','));
+		}
+		//------------------------------------------------
+		//CSVのデータを_cardDatasへ格納-------------------------------------------------------
+		for (int i = 0; i < csvStringArrayList.Count; i++) {
 			if ( i > 0 ) {//i==0は各項目名なので除く
 				CardData cardData = new CardData();
-				cardData._id 					= int.Parse(_csvStringData [i] [0]);
-				cardData._attack 				= int.Parse(_csvStringData [i] [1]);
-				cardData._toughness 			= int.Parse(_csvStringData [i] [2]);
-				cardData._directionOfTravel 	= int.Parse(_csvStringData [i] [3]);
-				cardData._effect 				= int.Parse(_csvStringData [i] [4]);
-				//_cardDatas [i - 1]._necessaryMP = int.Parse(_csvStringData [i] [5]);
-				cardData._necessaryAP 			= int.Parse(_csvStringData [i] [5]);
-				cardData._necessaryAPForEffect 	= int.Parse(_csvStringData [i] [6]);
+				cardData._id 					= int.Parse(csvStringArrayList [i] [0]);
+				cardData._attack 				= int.Parse(csvStringArrayList [i] [1]);
+				cardData._toughness 			= int.Parse(csvStringArrayList [i] [2]);
+				cardData._directionOfTravel 	= int.Parse(csvStringArrayList [i] [3]);
+				cardData._effect 				= int.Parse(csvStringArrayList [i] [4]);
+				cardData._necessaryMP 			= int.Parse(csvStringArrayList [i] [5]);
+				cardData._necessaryAP 			= int.Parse(csvStringArrayList [i] [5]);
+				cardData._necessaryAPForEffect 	= int.Parse(csvStringArrayList [i] [6]);
 				_cardDatas.Add(cardData);
 			}
 		}
+		//------------------------------------------------------------------------------------
 	}
 
-	// Use this for initialization
-	void Start () {
-		
+
+	//==================================================================================================
+	//public関数
+
+	//--カードIDがidのカードデータを返す関数
+	public CardData GetCardDataFromID( int id ) {
+		CardData value = null;
+		for (int i = 0; i < _cardDatas.Count; i++) {
+			if (_cardDatas [i]._id == id) {
+				value = _cardDatas [i];
+				break;
+			}
+		}
+		return value;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+	//==================================================================================================
+	//==================================================================================================
 }
--- WarConVer.TGS/Assets/CardMain.cs
+++ WarConVer.TGS/Assets/CardMain.cs
@@ -6,20 +6,26 @@ using UnityEngine;
 //
 //==使用方法：カードのGameObjectにアタッチ
 public class CardMain : MonoBehaviour {
-	[SerializeField] int _loadID = 0;	//読み込むカードID
+	[SerializeField] int _loadID = 0;							//読み込むカードID
 	[SerializeField] SpriteRenderer _cardSpriteRenderer = null;	//カードのSpriteRenderer
 	[SerializeField] CardDataLoader _cardDataLoader = null;
 	[SerializeField] CardData _cardData = null;
-	[SerializeField] int _actionCount = 0;		//移動回数
+	[SerializeField] int _actionCount = 0;						//移動回数
+	const int _MAX_ACTION_COUNT = 3;							//移動回数の最大値
 
 	// Use this for initialization
 	void Start () {
 		_cardSpriteRenderer = GetComponent<SpriteRenderer> ();
+		_cardSpriteRenderer.sprite = (Sprite)Resources.Load<Sprite> ("Card/" + _loadID);//カード画像の読み込み
 		_cardDataLoader = GameObject.Find ("CardDataLoader").GetComponent<CardDataLoader>();
-		Debug.Log (_loadID);
+		Load ();//カードデータの読み込み
 		Debug.Log (_cardData);
 		Debug.Log (_actionCount);
-		Debug.Log (_cardSpriteRenderer);
-		Debug.Log (_cardDataLoader);
+	}
+
+
+	//--カードデータを読み込む関数
+	void Load() {
+		_cardData = _cardDataLoader.GetCardDataFromID ( _loadID );
 	}
 }
