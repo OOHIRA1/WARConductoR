c44cdb4805cab83b93df5397050a043364ba60c1
oohira <t3e0vbbdwbd0zr6bgftu@docomo.ne.jp>
2018-12-06 04:09:29 +0900

    タスク：カードをデッキ枚数分一枚ずつ表示を変えられるようにする
    大平
    ・テストシーン上でAボタンを押したらカードを一枚ずつ表示するようにしました
    ・各カードIDによって画像やカードデータが変わるようになっています

---
+++ WarConVer.TGS/Assets/TestMainOohiraManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMainOohiraManager : MonoBehaviour {
+	public int _cardDisplayedCount = 0;//表示されたカードの数
+	public GameObject[] _cards = null;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.A)) {
+			if (_cardDisplayedCount < _cards.Length) {
+				_cards [_cardDisplayedCount++].SetActive (true);
+			}
+		}
+	}
+}
