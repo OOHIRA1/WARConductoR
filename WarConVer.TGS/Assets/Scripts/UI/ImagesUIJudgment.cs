4c32fb51b740d1edb269c80cae216204a2aa6957
tokimamiya <ymjftokinotabibito@gmail.com>
2018-12-18 19:58:25 +0900

    あげなおし

---
+++ WarConVer.TGS/Assets/Scripts/UI/ImagesUIJudgment.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImagesUIJudgment : ImagesDependedOnNumber {
+
+    public Sprite _image1;
+    public Sprite _image2;
+
+    public override void UpdateteImages()
+    {
+        this.gameObject.GetComponent<Image> ();
+
+        if (Input.GetMouseButtonDown(0)) {
+
+
+        }
+
+
+    }
+    /*	// Use this for initialization
+    //	void Start () {
+    //		
+    //	}
+    //	
+    //	// Update is called once per frame
+    //	void Update () {
+    //		
+        }
+    */
+}
