2bdc40aae758d3e7d4026a932852ef05376f3852
Iwakudaki <workpc115@outlook.jp>
2019-01-08 15:07:58 +0900

    コミットしなおし

---
+++ WarConVer.TGS/Assets/Scripts/NetWork/DisableNetworkUnlocalplayerBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class DisableNetworkUnlocalplayerBehaviour : NetworkBehaviour {
+
+	[ SerializeField ] Behaviour[ ] _behaviours;
+
+	void Start( ) {
+		if ( !isServer ) { 
+			foreach( var behaviour in _behaviours ) { 
+				behaviour.enabled = false;	
+			}
+		}
+	}
+
+}
