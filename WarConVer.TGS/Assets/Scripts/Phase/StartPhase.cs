c7de26d254d04bd1f2faf5b21c9117c3211d45ce
oohira <t3e0vbbdwbd0zr6bgftu@docomo.ne.jp>
2019-01-12 03:35:57 +0900

    メインシーンの編集、ターンロゴカットイン機能追加
    ・メインシーンのBGMが流れるようにしました
    ・ターン開始時にカットインが流れるようにしました
    ・ターンロゴ画像を追加しました

--- WarConVer.TGS/Assets/Scripts/Phase/StartPhase.cs
+++ WarConVer.TGS/Assets/Scripts/Phase/StartPhase.cs
@@ -1,11 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StartPhase : Phase {
 	bool _didRefresh = false;
+	Animator _turnLogoAnimator;		//ターン開始時に流れるUIのAnimator
+
 	public StartPhase( Participant turnPlayer ) {
 		_turnPlayer = turnPlayer;
+
+		GameObject turnLogo = GameObject.Find ( "TurnLogo" );
+		//ターンロゴSpriteの更新----------------------------------------------------
+		Image turnLogoImage = turnLogo.GetComponent<Image> ( );
+		if ( _turnPlayer.gameObject.tag == "Player1" ) {
+			turnLogoImage.sprite = Resources.Load<Sprite> ( "UI/ui_your_turn" );
+		} else {
+			turnLogoImage.sprite = Resources.Load<Sprite> ( "UI/ui_enemy_turn" );
+		}
+		//--------------------------------------------------------------------------
+		_turnLogoAnimator = turnLogo.GetComponent<Animator>();
+
+		_turnLogoAnimator.SetTrigger ( "cutinTrigger" );
 
 		Debug.Log( _turnPlayer.gameObject.tag + "スタートフェーズ" );	
 	}
@@ -15,7 +31,13 @@ public class StartPhase : Phase {
 
 		_turnPlayer.Refresh( );
 		_turnPlayer.CardRefresh( );
-		_didRefresh = true;
+
+		int baseLayerIndex = _turnLogoAnimator.GetLayerIndex ("Base Layer");
+		AnimatorStateInfo stateInfo = _turnLogoAnimator.GetCurrentAnimatorStateInfo ( baseLayerIndex );
+		if ( stateInfo.IsName( "cutin" ) && stateInfo.normalizedTime >= 1.0f ) {//カットインが終了したら
+			_turnLogoAnimator.SetTrigger ( "returnIdleTrigger" );
+			_didRefresh = true;
+		}
 	}
 
 
