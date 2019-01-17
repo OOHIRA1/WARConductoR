81c95b127a14fcd4d06f6453bbaaffb9fd454f53
oohira <t3e0vbbdwbd0zr6bgftu@docomo.ne.jp>
2019-01-17 18:04:53 +0900

    戦闘アニメーションの作成
    ・戦闘背景画像を追加しました
    ・戦闘アニメーションを２つ（右が勝つor左が勝つ）作成しました
    ・テストシーンで、戦闘アニメーションをするオブジェクトをInstantiateし、左右のどっちが勝つかを指定することで、アニメーションを再生するようにしました
    ・現在、戦闘アニメーション後自動的にDestroyするようにしています

--- WarConVer.TGS/Assets/TestMainOohiraManager.cs
+++ WarConVer.TGS/Assets/TestMainOohiraManager.cs
@@ -10,6 +10,9 @@ public class TestMainOohiraManager : MonoBehaviour {
 	public AudioClip _clip;
 	public CardMain _card;
 	public GameObject _tapEffect;
+	public GameObject _battleSpacePrefab;
+	public AutoDestroyBattleSpace _battleSpace;
+	public Sprite[] _cardSprite;
 
 	// Use this for initialization
 	void Start () {
@@ -50,6 +53,16 @@ public class TestMainOohiraManager : MonoBehaviour {
 			Vector3 effectPosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
 			effectPosition.z = -9;
 			Instantiate (_tapEffect, effectPosition, Quaternion.identity);
+		}
+
+		if (Input.GetKeyDown (KeyCode.E)) {
+			GameObject battleSpaceObj = Instantiate (_battleSpacePrefab, Vector3.zero, Quaternion.identity);
+			AutoDestroyBattleSpace battleSpace = battleSpaceObj.GetComponent<AutoDestroyBattleSpace> ();
+			battleSpace.StartLeftWinAnim ( _cardSprite[0], _cardSprite[1] );
+		}
+		if (Input.GetKeyDown (KeyCode.F)) {
+			AutoDestroyBattleSpace battleSpace = Instantiate<AutoDestroyBattleSpace> (_battleSpace, Vector3.zero, Quaternion.identity);
+			battleSpace.StartRightWinAnim ( _cardSprite[0], _cardSprite[1] );
 		}
 	}
 }
